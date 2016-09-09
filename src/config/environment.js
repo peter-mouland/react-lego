2acaaf6713db50caf7eca079afab2be540f99391
Peter Mouland <peter.mouland@gmail.com>
2016-09-09 09:08:36 +0100

    move DEBUG setting to fix windows

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,21 +1,17 @@
 const debug = require('debug');
+const setEnvs = {};
+const setEnvDefault = (key, val) => {
+  if (!process.env[key]) {
+    process.env[key] = val;
+  }
+  setEnvs[key] = process.env[key];
+};
+
+setEnvDefault('DEBUG', 'lego:*');
+setEnvDefault('NODE_ENV', 'development');
+setEnvDefault('PORT', 3000);
+
 debug.enable(process.env.DEBUG);
 const log = debug('lego: Environment:');
 
-log({
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV
-});
-
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'development';
-}
-
-if (!process.env.PORT) {
-  process.env.PORT = 3000;
-}
-
-log({
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV
-});
+log(setEnvs);
