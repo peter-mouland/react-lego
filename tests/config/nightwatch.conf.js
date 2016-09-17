486a934acf7b358ded620b6eb9b22f0a1adf9a79
Peter Mouland <peter.mouland@gmail.com>
2016-09-17 10:53:41 +0100

    improve e2e test + add it to pre-push hook

--- tests/config/nightwatch.conf.js
+++ tests/config/nightwatch.conf.js
@@ -2,12 +2,17 @@
 const argv = require('yargs')
   .usage('Usage: $0 --target=[string] --sha=[string] --bskey=[string] --bsuser=[string]')
   .argv;
+process.env.PORT = 3210;
+require('../../src/config/environment');
 require('babel-core/register')({
   only: [/src/, /tests/, /config/]
 });
 require("babel-polyfill");
 const hook = require('node-hook').hook;
 hook('.scss', (source, filename) => `console.log("${filename}");`);
+
+const testServer = require('../../src/server/server.js');
+let openServer;
 
 module.exports = (function(settings) {
   var buildString = "";
@@ -19,7 +24,16 @@ module.exports = (function(settings) {
   }
 
   settings.test_settings.default.globals = {
-    TARGET_PATH : argv.target || 'http://localhost:3000'
+    TARGET_PATH : argv.target || `http://localhost:${process.env.PORT}`,
+    before: function(done) {
+      openServer = testServer.listen(process.env.PORT, () => {
+        console.log(`listening at http://localhost:${process.env.PORT}`); // eslint-disable-line
+        done()
+      });
+    },
+    after: function(done) {
+      return openServer.close(done);
+    }
   };
   settings.test_settings.default.desiredCapabilities['browserstack.user'] = argv.bsuser || process.env.BROWSERSTACK_USER;
   settings.test_settings.default.desiredCapabilities['browserstack.key'] = argv.bskey || process.env.BROWSERSTACK_KEY;
