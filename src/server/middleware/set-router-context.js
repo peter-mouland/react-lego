5c86bc50df48567877ee91607a27f31e075a9a97
Peter Mouland <peter.mouland@gmail.com>
2017-12-18 10:06:23 +0000

    remove graphql babel plugin

--- src/server/middleware/set-router-context.js
+++ src/server/middleware/set-router-context.js
@@ -3,21 +3,22 @@ import { renderToString } from 'react-dom/server';
 import { ApolloProvider, getDataFromTree, renderToStringWithData } from 'react-apollo';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
-// import { createHttpLink } from 'apollo-link-http';
-// import { SchemaLink } from 'apollo-link-schema';
+import { createHttpLink } from 'apollo-link-http';
+import { SchemaLink } from 'apollo-link-schema';
 import { withClientState } from 'apollo-link-state';
 
 import schema from '../../app/schema/schema';
 import Homepage from '../../app/containers/Homepage/Homepage';
 
-// const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT || '' // 'graphql';
-// const GRAPHQL_PORT = process.env.GRAPHQL_PORT || 3000;
-// const HOST = process.env.HOST || 'http://localhost';
-// const apiUrl = `${HOST}:${GRAPHQL_PORT}/${GRAPHQL_ENDPOINT}`;
+const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT || 'api' // 'graphql';
+const GRAPHQL_PORT = process.env.GRAPHQL_PORT || 3000;
+const HOST = process.env.HOST || 'http://localhost';
+const apiUrl = `${HOST}:${GRAPHQL_PORT}/${GRAPHQL_ENDPOINT}`;
 
 const createClient = ({ request, headers }) => new ApolloClient({
   ssrMode: true,
-  link: withClientState({ schema }),
+  link: new SchemaLink({ schema }),
+  // link: withClientState({ schema }),
   // link: createHttpLink({
   //   uri: apiUrl,
   //   credentials: 'same-origin',
@@ -39,15 +40,15 @@ function setRouterContext() {
     const App = Markup({ req: ctx.request, context: routerContext, client });
 
     // await getDataFromTree(App)
-    // await renderToStringWithData(App)
-    //   .then((markup) => {
-        // We are ready to render for real
-        const markup = renderToString(App);
+    //   .then(() => {
+    await renderToStringWithData(App)
+      .then((markup) => {
+        // const markup = renderToString(App);
         ctx.initialState = client.extract();
         ctx.status = 200;
         ctx.markup = markup;
-      // })
-      // .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
     await next();
   };
 }
